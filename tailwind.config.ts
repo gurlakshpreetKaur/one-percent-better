36373ef246216713dd605a51f25fef0f783cc6aa
gurlakshpreetKaur <gurlakshpreetkaur@gmail.com>
2024-01-02 22:43:36 +0530

    Add colors and font

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,12 +8,38 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
+      colors: {
+        "coffee": {
+          "50": "#f9f1ec",
+          "100": "#f3e2d8",
+          "200": "#e7c5b1",
+          "300": "#e1b69d",
+          "400": "#dba88a",
+          "500": "#d59976",
+          "600": "#cf8b63",
+          "700": "#c97c4f",
+          "800": "#c36d3c",
+          "900": "#b06336",
+          "1000": "#754224",
+          "1100": "#4e2c18"
+        },
+        "yerba-mate": {
+          "50": "#dbdcd6",
+          "100": "#c3c4ba",
+          "200": "#abad9f",
+          "300": "#939583",
+          "400": "#878a75",
+          "500": "#6c6e5e",
+          "600": "#515346",
+          "700": "#43453b",
+          "800": "#36372f"
+        },
+        "silver": "#E8E3E0"
+      }
     },
+    fontFamily: {
+      "noto": ["'Noto Serif'", "serif"]
+    }
   },
   plugins: [],
 }
