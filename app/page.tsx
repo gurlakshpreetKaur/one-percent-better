c97c133b9b1df004adb29bde7533649a12b07bda
gurlakshpreetKaur <gurlakshpreetkaur@gmail.com>
2024-01-05 14:20:14 +0530

    Improve upon responsive design

--- app/page.tsx
+++ app/page.tsx
@@ -18,17 +18,17 @@ export default function LandingPage() {
     flex justify-center items-center`}>
       <div className={`bg-white bg-opacity-60 
       sm:rounded-3xl sm:border-4 sm:border-solid sm:border-yerba-mate-600 
-      h-full w-full lg:w-4/5 md:aspect-square lg:aspect-auto p-12 overflow-y-auto
-      grid lg:grid-cols-[3fr_2fr] md:grid-cols-[repeat(auto-fill,minmax(250px,1fr))] gap-10`}>
-        <section className="flex flex-col">
+      h-full w-full max-h-[37.5rem] max-w-[55rem] lg:w-4/5 md:aspect-square lg:aspect-video p-12 overflow-y-auto
+      grid lg:grid-cols-[3fr_2fr] md:grid-cols-[repeat(auto-fill,minmax(15rem,1fr))] gap-10`}>
+        <section className="flex flex-col min-h-[50vh] md:min-h-fit">
           <h1 className="text-yerba-mate-600 font-semibold font-source lg:text-6xl md:text-5xl text-6xl text-center">{heading}</h1>
           <div className="flex flex-col justify-around h-full mt-4 md:mb-8">
             <p className="text-sm font-noto w-3/4 max-w-76 inline-block md:my-2"> {context} <em className="font-source text-yerba-mate-700">Small steps, everyday.</em> </p>
             <p className="text-sm text-right ml-auto w-3/4 max-w-76 inline-block md:my-2"><em className="font-source text-yerba-mate-700">{quote.text}</em> <br /> <span className="font-noto">{quote.writer}</span></p>
           </div>
         </section>
-        <section className="flex">
-          <div className="rounded-xl bg-white bg-opacity-60 hover:bg-yerba-mate-300 py-8 px-4 w-full h-full border-4 border-solid border-yerba-mate-600 text-center">
+        <section className="flex justify-center items-center">
+          <div className="rounded-xl bg-white bg-opacity-60 py-8 px-4 w-full h-min border-4 border-solid border-yerba-mate-600 text-center">
             <h1 className="text-yerba-mate-800 text-center text-xl font-source">Start Your Self-Improvement Journey</h1>
             <div className="flex flex-col justify-around mt-4 md:mb-8">
               <YerbaMateInput placeholder="Enter your name..." />
