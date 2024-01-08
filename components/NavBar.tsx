3eaf0a7160d2ada23095a535f59f4c0aafdf145f
gurlakshpreetKaur <gurlakshpreetkaur@gmail.com>
2024-01-08 23:43:52 +0530

    comment details

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -1,3 +1,7 @@
+/**
+ * Navbar for signed-in user to navigate through the site.
+ * Server-side rendered.
+ */
 export default function NavBar() {
     return (<nav className="w-full py-3 px-6 fixed top-0 left-0 right-0 bg-coffee-1000"></nav>);
 }
