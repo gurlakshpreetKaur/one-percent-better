f034c6cd6d63576e634524af8c65b24afd375993
gurlakshpreetKaur <gurlakshpreetkaur@gmail.com>
2024-01-09 22:01:30 +0530

    create to-do

---
+++ components/Widgets/ToDo.tsx
@@ -0,0 +1,8 @@
+export default function ToDo() {
+    return <div className="bg-yerba-mate-400 hover:bg-yerba-mate-500 rounded-lg p-5">
+        <h3 className="font-noto text-white font-semibold">To Do</h3>
+        <li>
+            <ul>Cook food</ul>
+        </li>
+    </div>
+}
