9cd2e5ff7bda858b1e2ecd5464b12cfb4c7e27d7
gurlakshpreetKaur <gurlakshpreetkaur@gmail.com>
2024-01-18 21:26:15 +0530

    Update tags for semantics

--- components/Widgets/WidgetContainer.tsx
+++ components/Widgets/WidgetContainer.tsx
@@ -1,3 +1,5 @@
+// import { AiOutlineInfoCircle } from "react-icons/ai"; <== will be used later to give info on each widget
+
 /**
  * A widget container with yerba-mate bg and hover effects.
  * Server-side rendered.
@@ -6,11 +8,12 @@
  * @param {boolean} noPadding Disables padding in the main content area of widget container, allowing the children to occupy full width and height
  */
 export default function WidgetContainer({ children, widgetName, noPadding = false }: { children: JSX.Element | JSX.Element[], widgetName: string, noPadding?: boolean }) {
-    return <div className="bg-yerba-mate-300 rounded-xl px-5 pt-4 pb-5 group">
-        <span className="inline-block mb-3 hover:cursor-pointer">
-            <h3 className="font-source text-yerba-mate-700  group-hover:text-white font-semibold text-xl">{widgetName}</h3>
-        </span>
-        <div className={`bg-white bg-opacity-60 group-hover:bg-opacity-100 transition-all rounded-lg hover:cursor-pointer ${noPadding ? "" : "p-3"}`}>
+    return <div className="bg-yerba-mate-300 rounded-xl px-5 pt-4 pb-5 group flex flex-col h-[50vh]">
+        <header className="inline-block mb-3 hover:cursor-pointer w-full">
+            <h3 className="font-source text-yerba-mate-700  group-hover:text-white font-semibold text-xl inline-block">{widgetName}</h3>
+        </header>
+        <div className={`bg-white bg-opacity-60 group-hover:bg-opacity-100 transition-all rounded-lg 
+        hover:cursor-pointer flex-grow ${noPadding ? "" : "px-3"} overflow-y-auto overflow-x-hidden`}>
             {children}
         </div>
     </div>
