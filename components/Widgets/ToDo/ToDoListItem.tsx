38052430727aed2d4eebe2f09008b7b10b27e7f0
gurlakshpreetKaur <gurlakshpreetkaur@gmail.com>
2024-01-22 15:25:08 +0530

    Add js-docs documentation/comments

--- components/Widgets/ToDo/ToDoListItem.tsx
+++ components/Widgets/ToDo/ToDoListItem.tsx
@@ -1,13 +1,23 @@
 "use client"
 import { useState } from "react";
 
+/**
+ * @property {string} task The task that the user has entered 
+ * @property {boolean} striked Specifies whether the task has been completed or not (true = has been completed)
+ * @property {number} dateCreated Specifies the date when the task was created
+ */
 interface ToDoListItemInterface {
+    /** The task that the user has entered  */
     task: string,
+    /**  Specifies whether the task has been completed or not (true = has been completed) */
     striked: boolean,
+    /** Specifies the date when the task was created */
     dateCreated: number,
 }
 /**
  * Creates each specific item of the to-do list. Used only as a child within to-do widget.
+ * @param {string} task The task that the user has entered
+ * @param {boolean} striked Specifies whether the task has been completed or not
  */
 export function ToDoListItem({ task, striked }: ToDoListItemInterface) {
     const [strikedState, setStrikedState] = useState(striked);
