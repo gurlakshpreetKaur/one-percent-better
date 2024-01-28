cb98a5c44ffd75022bd3af42171d4bd396eb0c6c
gurlakshpreetKaur <gurlakshpreetkaur@gmail.com>
2024-01-28 22:59:52 +0530

    Make timer fully functional

--- components/Widgets/FocusTimer.tsx
+++ components/Widgets/FocusTimer.tsx
@@ -1,7 +1,7 @@
 'use client'
 import { Dispatch, SetStateAction, useEffect, useState } from "react";
 import WidgetContainer from "./WidgetContainer";
-import { AiOutlinePlayCircle } from "react-icons/ai";
+import { AiOutlinePlayCircle, AiOutlineStop, AiOutlinePauseCircle } from "react-icons/ai";
 // import { clearInterval } from "timers";
 
 //TODO: Create pause option separately which doesn't reset timer.
@@ -16,11 +16,13 @@ export default function FocusTimer() {
     const [minutesTemp, setMinutesTemp] = useState(minutesFixed); //<= will be used them timer starts
     const [secondsTemp, setSecondsTemp] = useState(secondsFixed); //<= will be used them timer starts
     const [started, setStarted] = useState(false);
+    const [paused, setPaused] = useState(false);
 
-    /**Handles state for starting timer and calls the function that starts/pauses timer */
-    function manageTimer() {
+    /**Handles state for starting timer and calls the function that starts/stops timer */
+    function startOrStopTimer() {
         if (started) {
             setStarted(false);
+            setPaused(false);
         } else {
             setMinutesTemp(minutesFixed);
             setSecondsTemp(secondsFixed);
@@ -28,20 +30,35 @@ export default function FocusTimer() {
         }
     }
 
+    //this will be set off by the startOrStopTimer(). when timer is started, secondsTemp is set to value of seconds
+    // fixed. after that, useEffect will be triggered
     useEffect(() => {
-        if (started) setTimeout(() => startTimer(minutesTemp, secondsTemp), 1000);
-    }, [started]);
+        if (started && !paused) { //if timer is started
+            setTimeout(() => { //then we set timeout of 1000ms, calling start timer which will reduce no. of seconds by 1
+                startTimer(minutesTemp, secondsTemp, started); // as this will update seconds temp after 1000ms, then
+                //                                                once again useEffect will be called and so, this works sort of recursively
+            }, 1000)
+        }
+    }, [secondsTemp, paused]);
 
-    /**Recursive function that reduces second by 1, and sets timout of 1000ms for itself, until either start state becomes false, or minutes and seconds both become 0 (timer ends).
+    useEffect(() => {
+        console.log("isPaused?", paused);
+    }, [paused]);
+
+
+    /**A function that simply reduces seconds by 1, or if seconds===0 then sets seconds=59 and minutes=minutes-1
      * @param {number} minutes The minutes to use as starting point for the timer
      * @param {number} seconds The seconds to use as starting point for the timer
+     * @param {boolean} started current status of timer (started/paused) so that recursion can be ended
      * @description You may wonder why this function uses params, when it could just use the state. State is being passed statically, we could solve that with useRef, but I didn't want to both with that so I've deduced a clever work-around and used params which will update the next time the function is called, keeping them synced with the state.
      */
-    const startTimer = (minutes: number, seconds: number) => {
+    const startTimer = (minutes: number, seconds: number, started: boolean) => {
         if (!started || (minutes === 0 && seconds === 0)) {
             setStarted(false);
             return;
         }
+
+        console.log(minutes, seconds, started);
 
         if (seconds === 0) { //if seconds is 0, we've finished a minute, so seconds turns to 59, and mintues is reduced by 1
             seconds = 59;
@@ -52,8 +69,6 @@ export default function FocusTimer() {
             seconds--;
             setSecondsTemp(prev => prev - 1);
         }
-
-        setTimeout(() => startTimer(minutes, seconds), 1000);
     }
 
     return <WidgetContainer widgetName="Focus Timer">
@@ -67,8 +82,15 @@ export default function FocusTimer() {
                     placeholder="00" ariaPlaceholder="Enter timer seconds..." started={started} />
             </span>
             <br />
-            <button className={`bg-yerba-mate-600 hover:bg-yerba-mate-400 text-md p-2 mt-2 text-white rounded-full `}
-                onClick={manageTimer}><AiOutlinePlayCircle /></button>
+            {<button className={`${started ? "inline-block" : "hidden"}
+            ${!paused ? "bg-yerba-mate-500" : "bg-yerba-mate-700"}
+            hover:bg-yerba-mate-800  text-md p-2 mt-2 text-white rounded-full mx-2`}
+                onClick={() => setPaused(prev => !prev)}>
+                {paused ? <AiOutlinePlayCircle /> : <AiOutlinePauseCircle />}
+            </button>}
+            <button className={`${started ? "bg-red-500 hover:bg-red-600" : "bg-yerba-mate-600 hover:bg-yerba-mate-800"}
+            text-md p-2 mt-2 text-white rounded-full mx-2`}
+                onClick={startOrStopTimer}>{started ? <AiOutlineStop /> : <AiOutlinePlayCircle />}</button>
         </div>
     </WidgetContainer>
 }
@@ -104,7 +126,7 @@ interface FocusTimerNumberInputParams {
  */
 function FocusTimerNumberInput({ setState, placeholder, ariaPlaceholder, value, started }: FocusTimerNumberInputParams) {
     function formatDisplayFromValue() {
-        if (value < 9) return `0${value}`;
+        if (value <= 9) return `0${value}`;
         return `${value}`; // <== need to stringify it, else it has extra 0 in front (12 => 012, String(12) => 12)
     }
 
