4acdfe4fb2fb90c122d6fee7f297096f372a6bdf
gurlakshpreetKaur <gurlakshpreetkaur@gmail.com>
2024-01-30 22:16:20 +0530

    Add active indicator param

--- components/Widgets/WidgetContainer.tsx
+++ components/Widgets/WidgetContainer.tsx
@@ -7,13 +7,16 @@
  * @param {string} widgetName Name of the widget, creates a heading for the widget
  * @param {boolean} noPadding Disables padding in the main content area of widget container, allowing the children to occupy full width and height
  */
-export default function WidgetContainer({ children, widgetName, noPadding = false }: { children: JSX.Element | JSX.Element[], widgetName: string, noPadding?: boolean }) {
-    return <div className="bg-yerba-mate-300 rounded-xl px-5 pt-4 pb-5 group flex flex-col h-[50vh]">
+export default function WidgetContainer({ children, widgetName, noPadding = false, activeIndicator = false }: { children: JSX.Element | JSX.Element[], widgetName: string, noPadding?: boolean, activeIndicator?: boolean }) {
+    return <div className={`${activeIndicator ? "bg-coffee-700" : "bg-yerba-mate-300 hover:bg-yerba-mate-500"} 
+    rounded-xl px-5 pt-4 pb-5 group flex flex-col h-[50vh]`}>
         <header className="inline-block mb-3 hover:cursor-pointer w-full">
-            <h3 className="font-source text-yerba-mate-700  group-hover:text-white font-semibold text-xl inline-block">{widgetName}</h3>
+            <h3 className={`font-source group-hover:text-white font-semibold text-xl inline-block 
+            ${activeIndicator ? "text-white" : "text-yerba-mate-700"}`}>{widgetName}</h3>
         </header>
         <div className={`bg-white bg-opacity-60 group-hover:bg-opacity-100 transition-all rounded-lg 
-        hover:cursor-pointer flex-grow ${noPadding ? "" : "px-3"} overflow-y-auto overflow-x-hidden`}>
+        hover:cursor-pointer flex-grow ${noPadding ? "" : "px-3"} overflow-y-auto overflow-x-hidden
+        ${activeIndicator ? "bg-opacity-100" : ""}`}>
             {children}
         </div>
     </div>
