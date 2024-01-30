eafc1eb916531afe3e3e3bb5ad8e194ede13df87
gurlakshpreetKaur <gurlakshpreetkaur@gmail.com>
2024-01-30 22:16:50 +0530

    Change styles

--- components/Widgets/CustomWidget.tsx
+++ components/Widgets/CustomWidget.tsx
@@ -2,8 +2,8 @@ import WidgetContainer from "./WidgetContainer";
 
 export default function CustomWidget() {
     return <WidgetContainer widgetName="Custom" noPadding>
-        <div className="h-full w-full flex justify-between items-center bg-yerba-mate-600 border-[5px] border-dashed">
-            <button className="text-4xl text-white inline-block m-auto">+</button>
+        <div className="h-full w-full flex justify-between items-center bg-yerba-mate-600 hover:bg-white border-[5px] border-dashed hover:border-yerba-mate-700 group">
+            <button className="text-4xl text-white inline-block m-auto group-hover:text-yerba-mate-700">+</button>
         </div>
     </WidgetContainer>
 }
