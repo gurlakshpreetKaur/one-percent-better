54b8bf8d5623491b64d858c278ad30e2dd2ec840
gurlakshpreetKaur <gurlakshpreetkaur@gmail.com>
2024-01-30 17:36:27 +0530

    Edit styles (add z-index)

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -3,7 +3,7 @@
  * Server-side rendered.
  */
 export default function NavBar() {
-    return (<nav className="w-full py-3 px-6 fixed top-0 left-0 right-0 bg-black">
+    return (<nav className="w-full py-3 px-6 fixed top-0 left-0 right-0 bg-black z-[500]">
         <ul>
             <li className="font-noto text-white font-semibold tracking-[1px]">1% Better</li>
         </ul>
