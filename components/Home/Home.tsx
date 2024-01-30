9254fe2af0f588844e57f015ef8e86e751cba771
gurlakshpreetKaur <gurlakshpreetkaur@gmail.com>
2024-01-30 17:36:15 +0530

    Add custom widget option and isolate widget pane

--- components/Home/Home.tsx
+++ components/Home/Home.tsx
@@ -1,21 +1,31 @@
+import ResizeableDiv from "../ResizeableDiv";
+import CustomWidget from "../Widgets/CustomWidget";
 import FocusTimer from "../Widgets/FocusTimer";
 import Journal from "../Widgets/Journal";
 import NoteToSelf from "../Widgets/NoteToSelf";
 import Quote from "../Widgets/Quote";
 import ToDo from "../Widgets/ToDo/ToDo";
 
-export default function HomeMain() {
-    return <div className="w-full h-full grid grid-cols-[2fr_0fr] md:grid-cols-[2fr_5fr] mt-12">
-        <section className="bg-red min-h-screen"><h1>H</h1></section>
-        <section className={`bg-white min-h-screen py-1 px-12 text-center md:inline-block [&>*]:my-5`}>
-            <h2 className="text-xl text-yerba-mate-600 font-source font-semibold">Quick Bits</h2>
+function WidgetsPane() {
+    return (<ResizeableDiv className="w-2/3 min-w-[350px] fixed 
+    top-0 bottom-0 right-0 bg-white h-screen text-center md:inline-block [&>*]:my-5">
+        <section className={` `}>
+            <h2 className="text-xl text-yerba-mate-600 font-source font-semibold mb-4">Quick Bits</h2>
             <div className="grid grid-cols-[repeat(auto-fill,minmax(225px,1fr))] gap-[10px]">
                 <Quote />
                 <ToDo />
                 <NoteToSelf />
                 <Journal />
                 <FocusTimer />
+                <CustomWidget />
             </div>
         </section>
-    </div>
+    </ResizeableDiv>);
+}
+
+export default function HomeMain() {
+    return <div className="w-full h-full mt-12 overflow-hidden">
+        <section className="bg-red min-h-screen"><h1>H</h1></section>
+        <WidgetsPane />
+    </div >
 }
